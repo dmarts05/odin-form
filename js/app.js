af8a2d695b2df13f8168596b975be22dc761087d
dmarts05 <dmarts05@estudiantes.unileon.es>
2022-06-14 14:07:26 +0200

    Add fiinal form styling

--- js/app.js
+++ js/app.js
@@ -3,7 +3,11 @@ function checkSamePassword() {
   const confirm = document.querySelector('input[name=confirm-password]');
   if (confirm.value === password.value) {
     confirm.setCustomValidity('');
+    password.classList.remove('error');
+    confirm.classList.remove('error');
   } else {
     confirm.setCustomValidity('Passwords do not match');
+    password.classList.add('error');
+    confirm.classList.add('error');
   }
 }
